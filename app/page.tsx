0f7bcc8310732afc03591ed6f39dda44911624ac
swathysukumaran <swathysukumaran@gmail.com>
2026-04-07 20:56:49 -0700

    Style main section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   
+    <main className="relative bg-primary flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <h1>Portfolio</h1>
+      </div>
+    </main>
   );
 }
