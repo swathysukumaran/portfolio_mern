196e40edb79ba9911ad228b7fc712c901cf8d460
swathysukumaran <swathysukumaran@gmail.com>
2026-04-09 13:29:39 -0700

    Add grid background

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,19 +1,37 @@
 import React from "react";
 import { Spotlight } from "./ui/spotlight";
-
+import { cn } from "@/lib/utils";
 const Hero = () => {
   return (
-    <div className="pb-20 pt-36">
-      <div>
-        <Spotlight
-          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen "
-          fill="white"
-        />
+    <>
+      <div className="pb-20 pt-36">
+        <div>
+          <Spotlight
+            className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen "
+            fill="white"
+          />
 
-        <Spotlight className="top-10 left-full h-[80h] w-[50w]" fill="purple" />
+          <Spotlight
+            className="top-10 left-full h-[80h] w-[50w]"
+            fill="purple"
+          />
+        </div>
+        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
       </div>
-      <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
-    </div>
+
+      <div className="relative flex h-screen w-full items-center justify-center bg-white dark:bg-black-100">
+        <div
+          className={cn(
+            "absolute inset-0",
+            "[background-size:40px_40px]",
+            "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+            "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
+          )}
+        />
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100" />
+      </div>
+    </>
   );
 };
 
