72972697a1c69fdca9fef0f781d736240cfa463b
swathysukumaran <swathysukumaran@gmail.com>
2026-04-27 10:42:51 -0700

    Create grid items

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { BentoGrid, BentoGridItem } from "./ui/bento-grid";
 
 const Grid = () => {
-  return <div></div>;
+  return (
+    <section id="about">
+      <BentoGrid>
+        {[{ title: "Title1", description: "Desc1" }].map((item, i) => (
+          <BentoGridItem
+            id={item.id}
+            key={item.id}
+            title={item.title}
+            description={item.description}
+          />
+        ))}
+      </BentoGrid>
+    </section>
+  );
 };
 
 export default Grid;
