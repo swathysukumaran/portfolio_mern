920f3355e2f9c008a963c1dc1d307db12286da2d
swathysukumaran <swathysukumaran@gmail.com>
2026-04-27 10:46:54 -0700

    Create mock data file

---
+++ data/index.ts
@@ -0,0 +1,6 @@
+export const gridItems=[
+    {title:'Title1',description:'Desc1', id:1},
+     {title:'Title1',description:'Desc1', id:2},
+      {title:'Title1',description:'Desc1', id:3}
+       {title:'Title1',description:'Desc1', id:4}, {title:'Title1',description:'Desc1', id:5}
+]
